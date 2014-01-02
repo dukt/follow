34c4f20d213896a522756cfcd0a3c10b37e9c851
Benjamin David <ben@dukt.net>
2014-01-03 00:01:22 +0100

    translations

--- follow/translations/en_us.php
+++ follow/translations/en_us.php
@@ -4,15 +4,17 @@ return array(
 
     // On Follow
 
-    "follow.onfollow_heading"    => "When someone gets followed:",
-    "follow.onfollow_subject"    => "You have a new follower",
-    "follow.onfollow_body"    => "{{user.friendlyName}} has started following you.",
+    "follow.onfollow_settings_label"   => "Notify me when someone starts following me",
+    "follow.onfollow_heading"          => "When someone gets followed:",
+    "follow.onfollow_subject"          => "You have a new follower",
+    "follow.onfollow_body"             => "{{user.friendlyName}} has started following you.",
 
 
     // On New Entry
 
-    "follow.onnewentry_heading"    => "When someone I follow posts a new entry:",
-    "follow.onnewentry_subject"    => "Someone you follow has posted a new entry",
-    "follow.onnewentry_body"    => "{{user.friendlyName}} has posted a new entry:\n\n".
-                                    "{{(entry.url is defined ? entry.url : 'Entry ID:'~entry.id)}}",
+    "follow.onnewentry_settings_label" => "Notify me when someone I follow posts new entries",
+    "follow.onnewentry_heading"        => "When someone I follow posts a new entry:",
+    "follow.onnewentry_subject"        => "Someone you follow has posted a new entry",
+    "follow.onnewentry_body"           => "{{user.friendlyName}} has posted a new entry:\n\n".
+                                          "{{(entry.url is defined ? entry.url : 'Entry ID:'~entry.id)}}",
 );
