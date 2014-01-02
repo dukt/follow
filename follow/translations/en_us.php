bc409d476607bb5b0018e861fc967c2202ffa01b
Benjamin David <ben@dukt.net>
2014-01-02 11:03:14 +0100

    translations

---
+++ follow/translations/en_us.php
@@ -0,0 +1,18 @@
+<?php
+
+return array(
+
+    // On Follow
+
+    "follow.onfollow_heading"    => "When someone gets followed:",
+    "follow.onfollow_subject"    => "You have a new follower",
+    "follow.onfollow_body"    => "A user has started following you : {{user.username}}",
+
+
+    // On New Entry
+
+    "follow.onnewentry_heading"    => "When someone I follow posts a new entry:",
+    "follow.onnewentry_subject"    => "Someone you follow has posted a new entry",
+    "follow.onnewentry_body"    => "A user that you are following has posted a new entry :
+                - entry : {{(entry.url is defined ? entry.url : entry.id)}}",
+);
